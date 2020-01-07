854a4a75bd99bf741846bc372d172b3a54be9845
vinay-lodha <58178003+vinay-lodha@users.noreply.github.com>
2020-01-07 13:36:05 +0530

    a

--- main/src/main/java/greenbot/main/ApplicationImpl.java
+++ main/src/main/java/greenbot/main/ApplicationImpl.java
@@ -3,10 +3,12 @@ package greenbot.main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@EnableWebMvc
 @SpringBootApplication
 public class ApplicationImpl implements CommandLineRunner {
 
--- main/src/main/java/greenbot/main/Beans.java
+++ main/src/main/java/greenbot/main/Beans.java
@@ -2,6 +2,8 @@ package greenbot.main;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import greenbot.rule.model.utils.RuleResponseReducer;
 
@@ -9,7 +11,25 @@ import greenbot.rule.model.utils.RuleResponseReducer;
 public class Beans {
 
 	@Bean
-	public RuleResponseReducer getRuleResponseReducer(){
+	public RuleResponseReducer getRuleResponseReducer() {
 		return new RuleResponseReducer();
 	}
+	@Bean
+	WebMvcConfigurer configurer() {
+	   return new WebMvcConfigurer() {
+	      @Override
+	      public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	          registry
+	                .addResourceHandler("/resources/**")
+	                .addResourceLocations("classpath:/static/")
+	                              .setCachePeriod(0);
+	          
+	          registry
+              .addResourceHandler("/webjars/**")
+              .addResourceLocations("/webjars/")
+                            .setCachePeriod(0);
+	      }
+	   };
+	}
+
 }
