8227ca9398a6bf9cf8cca6efca79fb5c92437c23
vinay-lodha <58178003+vinay-lodha@users.noreply.github.com>
2020-01-09 04:35:40 +0530

    l

--- main/src/test/java/greenbot/main/rules/service/DevTagAnalyzerTest.java
+++ main/src/test/java/greenbot/main/rules/service/DevTagAnalyzerTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import greenbot.main.model.Tag;
 
 public class DevTagAnalyzerTest {
@@ -11,11 +14,11 @@ public class DevTagAnalyzerTest {
 	private DevTagAnalyzer devTagAnalyzer = new DevTagAnalyzer();
 	
 	
-	//@ParameterizedTest
-	//@CsvFileSource(resources = "/dev-tags.csv.csv")
+	@ParameterizedTest
+	@CsvFileSource(resources = "/dev-tags.csv")
 	void withCsvSource(String key, String value, Boolean outcome ) {
 		Tag tag = Tag.builder().key(key).value(value).build();
-		assertEquals(outcome, Arrays.asList(tag));
+		assertEquals(outcome, devTagAnalyzer.isDevTagPresent(Arrays.asList(tag)));
 	}
 
 
