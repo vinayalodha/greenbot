f0b58f368426d1f537510876a2ae68c8ef7a6712
vinay-lodha <58178003+vinay-lodha@users.noreply.github.com>
2020-02-28 23:42:36 +0530

    updated landing page footer and messages

--- ui/src/messages/AwsCliMessageComponent.tsx
+++ ui/src/messages/AwsCliMessageComponent.tsx
@@ -21,7 +21,7 @@ export class AwsCliMessageComponent extends Component<{}> {
         return (
             <div className="message">
                 <div className="message-body">
-                    Do make sure <a href="https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html" target="_blank" rel="noopener noreferrer">AWS cli</a> is configured with correct IAM roles
+                    Do make sure <a href="https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html" target="_blank" rel="noopener noreferrer">aws cli</a> is configured with correct IAM roles and you are using <a href='https://github.com/vinay-lodha/greenbot/releases'>most recent version</a> of Greenbot (current version <strong>v1.0.0</strong>).
                 </div>
             </div>
         );
--- ui/src/messages/FooterComponent.tsx
+++ ui/src/messages/FooterComponent.tsx
@@ -21,9 +21,13 @@ export class FooterComponent extends Component<{}> {
         return (
             <footer className="footer">
                 <div className="content has-text-centered">
-                    <p><strong>Maintained by <a href="https://www.linkedin.com/in/vinaylodha/" target='_blank' rel="noopener noreferrer">Vinay Lodha</a>
-                    <br/>
-                    <a href="mailto:vinay.a.lodha@gmail.com">Feedback</a> | <a target="_blank" href="https://github.com/vinay-lodha/greenbot/issues">Report Bug</a> </strong></p>
+                    <p>
+                        <strong>
+                            Maintained by <a href="https://www.linkedin.com/in/vinaylodha/" target='_blank' rel="noopener noreferrer">Vinay Lodha</a>
+                            <br/>
+                            <a href="mailto:vinay.a.lodha@gmail.com">Feedback</a> | <a target="_blank" rel="noopener noreferrer" href="https://github.com/vinay-lodha/greenbot/issues">Report Bug</a> 
+                        </strong>
+                    </p>
                 </div>
             </footer>
         );
