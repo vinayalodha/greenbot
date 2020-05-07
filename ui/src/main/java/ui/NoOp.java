998f18d56bfd8891142de739449125267c3b62cf
vinay-lodha <58178003+vinay-lodha@users.noreply.github.com>
2020-05-07 19:01:11 +0530

    #176 pom changes

---
+++ ui/src/main/java/ui/NoOp.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019-2020 Vinay Lodha (https://github.com/vinay-lodha)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ui;
+
+/**
+ * 
+ * https://stackoverflow.com/questions/40964500/maven-jar-plugin-3-0-2-error-you-have-to-use-a-classifier-to-attach-supplementa
+ * 
+ * @author Vinay Lodha
+ */
+public class NoOp {
+
+}
